f4e408fcb3d1a0476a8bcba8f87f0c4d30420f0e
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-08-06 11:31:32 -0300

    Gravando

--- app/src/main/java/br/senai/sp/jandira/imc_app_b/model/Pesagem.kt
+++
@@ -1,9 +0,0 @@
-package br.senai.sp.jandira.imc_app_b.model
-
-data class Pesagem(
-    var  id: Int = 0,
-    var  peso: Double = 0.0,
-    var  nivelAtividade: Int = 0,
-    var  dataPesagem: String?= null,
-    var  Usuario: Usuario
-)
